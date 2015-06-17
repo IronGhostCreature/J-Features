650fa621e8c27b775aa847d59c85a41c10fef12a
jcloudcoder@gmail.com <c0ngerfish>
2015-06-18 00:21:55 +0100

    prism base setup

---
+++ J.windows.Features/Bootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.MefExtensions;
+using Microsoft.Practices.ServiceLocation;
+
+namespace J.windows.Features
+{
+    class Bootstrapper:MefBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return new Shell();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+            Application.Current.MainWindow = (Window)this.Shell;
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+        }
+    }
+}
